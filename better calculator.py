d06627f06067401f779e88e30b9b2bb606891540
ShAh-321 <mukti007@gmail.com>
2020-06-14 08:11:58 -0400

    Update better calculator.py

--- better calculator.py
+++ better calculator.py
@@ -1,21 +1,11 @@
 import turtle
 
 wn = turtle.Screen()
-wn.title("Letter to Arnav Tripathi")
-wn.bgcolor("forestgreen")
-wn.setup(width=900, height=600)
+wn.title("Stickman 1.1")
+wn.bgcolor("lightblue")
+wn.setup(width=600, height=800)
 wn.tracer(0)
 
-iv = turtle.Turtle()
-iv.color("white")
-iv.penup()
-iv.hideturtle()
-iv.goto(0,160)
-iv.write("But at least i have a life unlike u! ", align="center", font=("Courier", 30, "normal"))
-
-ik = turtle.Turtle()
-ik.color("white")
-ik.penup()
-ik.hideturtle()
-ik.goto(0, 100)
-ik.write("Just like a ming chong and ding dong! ", align="center", font=("Courier", 30, "normal"))
+head = turtle.Turtle()
+head.shape("circle")
+head.
